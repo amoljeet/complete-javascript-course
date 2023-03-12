c76a42dbb953a1a1e3c20f2e23c8cc3b1f4eda59
Amol <amoljeet>
2023-03-12 21:26:56 +0530

    128 - Default Parameters exercise added

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+// ###### Default Parameters ######
+console.log('##### Default Parameters ######');
+
+const bookings = [];
+const createBooking = function (
+  flightNum,
+  passengers = 1,
+  price = passengers * 500
+) {
+  const booking = {
+    flightNum,
+    passengers,
+    price,
+  };
+  bookings.push(booking);
+  console.log(
+    `Booking for ${passengers} passengers for flight num ${flightNum} at ${price} is confirmed `
+  );
+};
+
+createBooking('IN123');
+createBooking('IN123', 2, 100);
+createBooking('IN123', 5);
