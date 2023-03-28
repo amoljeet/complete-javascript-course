77d72a6de651f620179020d8906a75ef4d0bc9da
Amol <amoljeet>
2023-03-28 23:51:52 +0530

    177 - Operation with dates

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -412,3 +412,16 @@ console.log('##### Date #####');
 
 const now = new Date();
 console.log('Now = ', now);
+
+/**
+ * Operations with dates
+ */
+
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(+future);
+
+const calcDaysPassed = (date1, date2) =>
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
+console.log('No of days b/w = ', days1);
