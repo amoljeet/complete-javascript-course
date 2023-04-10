d53732599a97b2b1123578851796abbdfeb149cd
Amol <amoljeet>
2023-04-10 23:20:54 +0530

    214 - Getters and Setters

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -149,3 +149,25 @@ megGriffin.greet();
  * 2 - classes are 1st class citizens => we can pass them into functions and return from functions
  * 3 - classes are executed in strict mode
  */
+
+/**
+ * Setters and Getters
+ */
+
+const account = {
+  owner: 'Peter',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
